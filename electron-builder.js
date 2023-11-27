24082cf6b326811fa1ce2eda36e8fcaa4509d74e
fiahfy <d.fiahfy@gmail.com>
2023-11-27 23:56:40 +0900

    Update build config

--- electron-builder.js
+++ electron-builder.js
@@ -3,6 +3,9 @@ const imageExtensions = require('image-extensions')
 module.exports = {
   appId: 'net.fiahfy.photy',
   asar: true,
+  directories: {
+    output: 'dist-package',
+  },
   files: ['dist', 'dist-electron'],
   // @see https://github.com/electron-userland/electron-builder/issues/3204
   fileAssociations: [
